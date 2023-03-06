182f6395ae03b27f637c41f8ec259954f3554d15
PaulHaze <dev@artemist.com.au>
2023-03-07 08:25:49 +1100

    final lint config setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,21 +13,19 @@ const config = {
   },
   overrides: [
     {
+      files: ['*.ts', '*.tsx'],
       extends: [
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
-      files: ['*.ts', '*.tsx'],
+      plugins: ['unused-imports', 'import'],
       parserOptions: {
         project: 'tsconfig.json',
         tsconfigRootDir: __dirname,
         sourceType: 'module',
       },
       rules: {
-        'react/destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
-        'jsx-a11y/anchor-is-valid': 'off', // Next.js uses its own internal link system
         'react/require-default-props': 'off', // Allow non-defined react props as undefined
         'react/jsx-props-no-spreading': 'off', // _app.tsx uses spread operator and also, react-hook-form
-        'react-hooks/exhaustive-deps': 'off', // Incorrectly report needed dependency with Next.js router
         '@next/next/no-img-element': 'off', // We currently not using next/image because it isn't supported with SSG mode
         '@typescript-eslint/comma-dangle': 'off', // Avoid conflict rule between Eslint and Prettier
         '@typescript-eslint/consistent-type-imports': 'error', // Ensure `import type` is used when it's necessary
@@ -48,6 +46,7 @@ const config = {
           { devDependencies: true },
         ],
         '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
       },
     },
   ],
