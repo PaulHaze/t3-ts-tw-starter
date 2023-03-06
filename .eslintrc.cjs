9bc123ad4ae475b8133cbde46572700606a35fca
PaulHaze <dev@artemist.com.au>
2023-03-07 05:34:15 +1100

    more config setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,56 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'warn',
+  },
   overrides: [
     {
       extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
-      files: ["*.ts", "*.tsx"],
+      files: ['*.ts', '*.tsx'],
       parserOptions: {
-        project: "tsconfig.json",
+        project: 'tsconfig.json',
         tsconfigRootDir: __dirname,
-        sourceType: "module",
+        sourceType: 'module',
+      },
+      rules: {
+        'react/destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+        'jsx-a11y/anchor-is-valid': 'off', // Next.js uses its own internal link system
+        'react/require-default-props': 'off', // Allow non-defined react props as undefined
+        'react/jsx-props-no-spreading': 'off', // _app.tsx uses spread operator and also, react-hook-form
+        'react-hooks/exhaustive-deps': 'off', // Incorrectly report needed dependency with Next.js router
+        '@next/next/no-img-element': 'off', // We currently not using next/image because it isn't supported with SSG mode
+        '@typescript-eslint/comma-dangle': 'off', // Avoid conflict rule between Eslint and Prettier
+        '@typescript-eslint/consistent-type-imports': 'error', // Ensure `import type` is used when it's necessary
+        'import/prefer-default-export': 'off', // Named export is easier to refactor automatically
+        'import/extensions': [
+          'warn',
+          'ignorePackages',
+          {
+            '': 'never',
+            ts: 'never',
+            tsx: 'never',
+            js: 'never',
+            jsx: 'never',
+          },
+        ],
+        'import/no-extraneous-dependencies': [
+          'error',
+          { devDependencies: true },
+        ],
+        '@typescript-eslint/no-unused-vars': 'off',
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/consistent-type-imports": "warn",
-  },
 };
 
 module.exports = config;
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
 
